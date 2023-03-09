df9eb82732e01518d1331212131c1046011dda1f
Protus <prongugi@gmail.com>
2023-03-09 19:57:11 +0300

    merge done

--- models/images.js
+++ models/images.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// create a schema for our    database.
+// create a schema for our database.
 var imageSchema = new Schema({
     name: String,
     path: String,
